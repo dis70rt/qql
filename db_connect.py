fb71125df8679468bb32226f1f6818ec9fefb786
dis70rt <sd.saikat369@gmail.com>
2025-09-12 22:05:49 +0530

    feat: added connection to psql & persistence storage of credentials

---
+++ db_connect.py
@@ -0,0 +1,80 @@
+import psycopg2
+import os
+from pathlib import Path
+
+class PSQL:
+    def __init__(self, dbname, user, password, host, port):
+        self.conn = psycopg2.connect(
+            dbname=dbname,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+        self.cur = self.conn.cursor()
+
+    def execute(self, query, params=None):
+        self.cur.execute(query, params or ())
+        self.conn.commit()
+        return self.cur
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
+
+
+class QQLEnv:
+    ENV_FILE = Path.home() / ".qql_env"
+
+    def __init__(self):
+        self.username = None
+        self.password = None
+        self.dbname = None
+        self.host = "localhost"
+        self.port = 5432
+        self.load()
+
+    def load(self):
+        if self.ENV_FILE.exists():
+            with open(self.ENV_FILE) as f:
+                for line in f:
+                    if "=" in line:
+                        key, val = line.strip().split("=", 1)
+                        os.environ[key] = val
+
+        self.username = os.environ.get("DB_USERNAME")
+        self.password = os.environ.get("DB_PASSWORD")
+        self.dbname = os.environ.get("DB_NAME")
+        self.host = os.environ.get("DB_HOST", "localhost")
+        port = os.environ.get("DB_PORT")
+        self.port = int(port) if port else 5432
+
+    def save(self, username, password, dbname=None, host=None, port=None):
+        dbname = dbname or self.dbname
+        host = host or self.host
+        port = port or self.port
+
+        with open(self.ENV_FILE, "w") as f:
+            f.write(f"DB_USERNAME={username}\n")
+            f.write(f"DB_PASSWORD={password}\n")
+            if dbname:
+                f.write(f"DB_NAME={dbname}\n")
+            if host:
+                f.write(f"DB_HOST={host}\n")
+            if port:
+                f.write(f"DB_PORT={port}\n")
+
+        os.environ["DB_USERNAME"] = username
+        os.environ["DB_PASSWORD"] = password
+        if dbname:
+            os.environ["DB_NAME"] = dbname
+        if host:
+            os.environ["DB_HOST"] = host
+        if port:
+            os.environ["DB_PORT"] = str(port)
+
+        self.username = username
+        self.password = password
+        self.dbname = dbname
+        self.host = host
+        self.port = port
--- qql.py
+++ qql.py
@@ -1,10 +1,25 @@
 import cmd
 import sqlglot
 from utils.parser import QQL
+import click
+from db_connect import PSQL, QQLEnv
+
 class QQLShell(cmd.Cmd):
     intro = "Type exit to quit.\n"
     prompt = "qql> "
     buffer = ""
+
+    def __init__(self, username, password, dbname, host,port):
+        super().__init__()
+        self.username = username
+        self.password = password
+        self.db = PSQL(
+            dbname=dbname,
+            host=host,
+            password=password,
+            user=username,
+            port=port
+            )
 
     def default(self, line):
         self.buffer += " " + line.strip()
@@ -17,8 +32,6 @@ class QQLShell(cmd.Cmd):
     def execute_sql(self, statement):
         try:
             exp = sqlglot.parse_one(statement, read=QQL)
-            print(repr(exp))
-
         except Exception as e:
             print(f"An error occurred: {e}")
         
@@ -27,5 +40,37 @@ class QQLShell(cmd.Cmd):
     do_quit = do_exit
 
 
+@click.command()
+@click.option("--username", help="Database username")
+@click.option("--password", help="Database password")
+@click.option("--dbname", help="Database name")
+@click.option("--host", default="localhost", help="Database host")
+@click.option("--port", default=5432, help="Database port")
+def login(username, password, dbname, host, port):
+    env = QQLEnv()
+
+    username = username or env.username
+    password = password or env.password
+    dbname = dbname or getattr(env, "dbname", None)
+    host = host or getattr(env, "host", "localhost")
+    port = port or getattr(env, "port", 5432)
+
+    if not username:
+        username = click.prompt("Enter database username")
+    if not password:
+        password = click.prompt("Enter database password", hide_input=True)
+    if not dbname:
+        dbname = click.prompt("Enter database name")
+
+    env.save(username, password)
+    env.dbname = dbname
+    env.host = host
+    env.port = port
+    env.save(username, password)
+
+    shell = QQLShell(username, password, dbname, host, port)
+    shell.cmdloop()
+
+
 if __name__ == "__main__":
-    QQLShell().cmdloop()
+    login()
